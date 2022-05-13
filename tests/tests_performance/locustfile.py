20a84feda8bd7b1a7c66ad12c59554ecb6d87738
Cisco 1964 <francis.cazeres@gmail.com>
2022-05-13 15:05:14 +0200

    commit

--- tests/tests_performance/locustfile.py
+++ tests/tests_performance/locustfile.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from turtle import delay
 from locust import HttpUser, task, between
 
 class ProjectPerfTest(HttpUser):
@@ -10,15 +13,16 @@ class ProjectPerfTest(HttpUser):
     ]
     club =  [
         {
-            "name": "Iron Temple",
-            "email": "admin@irontemple.com",
-            "points":"13"
+            "name":"She Lifts",
+            "email": "kate@shelifts.co.uk",
+            "points":"12"
         }
     ]
 
     def on_start(self):
         self.client.get('/')
-        response = self.client.post('/showSummary', {'email': self.club[0]["email"]})
+        response = self.client.post('/showSummary', {'email': self.club[0]["email"]}, 
+            name="/showSummary")
         print("RESPONSE", response.status_code)
         
 
@@ -27,24 +31,45 @@ class ProjectPerfTest(HttpUser):
 
 
     @task(1)
-    def book(self):
-        rv = self.client.get(
-            #f"/book/{self.competitions[0]['name']}/{self.club[0]['name']}"
-            f"/book/SpringFestival/IronTemple"
-        )
-        print("response1", rv.status_code)
-        print("content1", rv.content)
+    def booking(self):
+        #s = f"/book/{self.competitions[0]['name']}/{self.club[0]['name']}"
+        #s = s.replace('%20',' ')
+        #rv = self.client.get(
+        #        s, 
+        #        name="book"
+        #)
+        rv = "/book/Spring Festival/She Lifts"
+        rv = rv.replace('%20', ' ')
+        #self.client.get(rv, name=rv)
+        
+        #print("RV", rv.status_code)
+        #print("type RV", type(rv))
+        with self.client.get("/book/Spring Festival/She Lifts", catch_response=True) as response:
+            if response.status_code != 401:
+                response.failure("Got unexpected response code: " + str(response.status_code) + " Error: " + str(response.text))
+            else:
+                print("401 Test passed")
+        response.success() 
 
     @task(1)
-    def purchase(self):
-        self.client.post(
-            "/purchasePlaces",
-            data={
-                "places": 1,
-                "club": self.club[0]["name"],
-                "competition": self.competitions[0]["name"]
-            }
-        )
+    def purchase(self): 
+        #self.client.post(
+          #  "/purchasePlaces",
+            #data={"places": 5, 
+                  #"club": self.club[0]["name"], 
+                  #"club": "She Lifts", 
+                  #"competition": self.competitions[0]["name"]}, 
+                   #"competition": "Spring Festival"},
+            #name="/purchasePlaces"
+        #)
+        with self.client.post("/purchasePlaces", data={"places": 5, 
+                  "club": "She Lifts", 
+                   "competition": "Spring Festival"}, catch_response=True) as response:
+            if response.status_code != 401:
+                response.failure("Got unexpected response code: " + str(response.status_code) + " Error: " + str(response.text))
+            else:
+                print("401 Test passed")
+        response.success() 
 
 class WebsiteUser(HttpUser):
     task_set = ProjectPerfTest
