36b423c15e25b66709b976ad74ab663c8e2c10f4
Cisco 1964 <francis.cazeres@gmail.com>
2022-05-16 18:10:12 +0200

    commit

--- tests/tests_unit/test_unknown_email.py
+++ tests/tests_unit/test_unknown_email.py
@@ -16,11 +16,6 @@ class TestUnknownEmail:
     def setup_method(self):
         server.clubs = self.club
 
-    def test_valid_email(self):
-        result = self.client.post("/showSummary", data={"email": server.clubs[0]["email"]})
-        assert result.status_code == 200
-        assert f"{server.clubs[0]['email']}" in result.data.decode()
-
     def test_invalid_email(self):
         result = self.client.post("/showSummary", data={"email": "toto"})
         assert result.status_code == 401
@@ -30,3 +25,4 @@ class TestUnknownEmail:
         result = self.client.post("/showSummary", data={"email": ""})
         assert result.status_code == 401
         assert "Veuillez saisir votre adresse mail" in result.data.decode()
+        
