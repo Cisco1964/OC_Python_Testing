315c657f2a325b1051a9ee7539191256c78d753c
Cisco 1964 <francis.cazeres@gmail.com>
2022-06-01 09:19:15 +0200

    commit

--- tests/tests_unit/test_points_allowed.py
+++ tests/tests_unit/test_points_allowed.py
@@ -24,12 +24,41 @@ class TestMorePoints:
         server.competitions = self.competition
         server.clubs = self.club
 
-
-    def test_more_points(self):
+    def test_points(self):
+        placesRequired = 1 
         rv = self.client.post(
             "/purchasePlaces",
             data={
-                "places": 14,
+                "places": placesRequired,
+                "club": server.clubs[0]["name"],
+                "competition": server.competitions[0]["name"]
+            }
+        )
+        assert rv.status_code == 200
+        assert "Great-booking complete!" in rv.data.decode()
+
+
+    def test_add_points(self):
+        placesRequired = 5 
+        rv = self.client.post(
+            "/purchasePlaces",
+            data={
+                "places": placesRequired,
+                "club": self.club[0]["name"],
+                "competition": self.competition[0]["name"]
+            }
+        )
+
+        assert rv.status_code == 400
+        assert "Vous ne pouvez pas reserver plus de 12 places" in rv.data.decode()
+        assert int(self.club[0]["points"]) >= 0
+
+    def test_more_points(self):
+        placesRequired = 14 
+        rv = self.client.post(
+            "/purchasePlaces",
+            data={
+                "places": placesRequired,
                 "club": self.club[0]["name"],
                 "competition": self.competition[0]["name"]
             }
@@ -37,5 +66,4 @@ class TestMorePoints:
 
         assert rv.status_code == 400
         assert "Pas assez de place disponible" in rv.data.decode()
-        assert int(self.club[0]["points"]) >= 0
         
