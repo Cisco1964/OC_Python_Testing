5e541642d597f2ee2d131d3c864502bd5a4390b1
Cisco 1964 <francis.cazeres@gmail.com>
2022-07-20 17:30:36 +0200

    test_points_allowed.py

--- tests/tests_unit/test_points_allowed.py
+++ tests/tests_unit/test_points_allowed.py
@@ -2,40 +2,65 @@ import server
 from server import app
 
 
-class TestMorePoints:
+class TestMoreThanTwelvePoints:
     client = app.test_client()
     competition = [
         {
-            "name": "Competition",
-            "date": "2020-03-27 10:00:00",
-            "numberOfPlaces": "25"
+            "name": "Test",
+            "date": "2020-04-22 10:30:00",
+            "numberOfPlaces": "20"
         }
     ]
 
     club = [
         {
-            "name": "Club",
-            "email": "toto@gmail.com",
-            "points": "13"
+            "name": "Test club",
+            "email": "test@email.com",
+            "points": "12"
+        }
+    ]
+
+    places_booked = [
+        {
+            "competition": "Test",
+            "booked": [5, "Test club"]
         }
     ]
 
     def setup_method(self):
         server.competitions = self.competition
         server.clubs = self.club
+        server.places_booked = self.places_booked
 
+    def test_less_than_twelve(self):
+        booked = 5
 
-    def test_more_points(self):
-        rv = self.client.post(
+        result = self.client.post(
             "/purchasePlaces",
             data={
-                "places": 14,
+                "places": booked,
                 "club": self.club[0]["name"],
                 "competition": self.competition[0]["name"]
             }
         )
 
-        assert rv.status_code == 400
-        assert "Pas assez de place disponible" in rv.data.decode()
+        #assert result.status_code == 200
+        assert "Great-booking complete!" in result.data.decode()
         assert int(self.club[0]["points"]) >= 0
-        
+
+    def test_more_than_twelve_added(self):
+        booked = 13
+
+        result = self.client.post(
+            "/purchasePlaces",
+            data={
+                "places": booked,
+                "club": self.club[0]["name"],
+                "competition": self.competition[0]["name"]
+            }
+        )
+
+        assert result.status_code == 400
+        assert "Pas assez de place disponible" in result.data.decode()  
+        assert int(self.club[0]["points"]) >= 0
+       
